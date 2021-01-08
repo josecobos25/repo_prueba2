7ebbf9471323ffe7aa7b5981f13dac4512f90c90
JoseA <jose.cobos@wiseitgroup.com>
2021-01-08 13:23:09 -0600

    Nueva modif a login

--- Login.php
+++ Login.php
@@ -6,7 +6,7 @@
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     
-    <title>Login Correspondencia Estadía</title>
+    <title>Login Correspondencia Estadía Profesional</title>
 </head >
 <body style="background-image: url(Imagenes/fondoPinos.jpg); background-size: cover; background-repeat: no-repeat;">
 
